1ac57a75e261a137f71a4fcd9c854ea561a0b17b
hexinping <hexinping456@126.com>
2018-10-08 13:40:13 +0800

    创建绘制窗口

--- project/OpenGL/OpenGL/main.cpp
+++ project/OpenGL/OpenGL/main.cpp
@@ -1,13 +1,64 @@
 
 #include "main.h"
 #include <iostream>
-#include "GLFW//glfw3.h"
-
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 using namespace std;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main(int argc, char* argv[])
 {
-	cout << "1-------" << endl;
-	system("pause");
+	glfwInit();
+
+	// 设置opegl版本号为3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//同样明确告诉GLFW我们使用的是核心模式(Core-profile) 明确告诉GLFW我们需要使用核心模式意味着我们只能使用OpenGL功能的一个子集
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+	int width = 800;
+	int heght = 600;
+
+	GLFWwindow* window = glfwCreateWindow(width, heght, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, width, heght);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	/*
+		glfwWindowShouldClose函数在我们每次循环的开始前检查一次GLFW是否被要求退出，如果是的话该函数返回true然后渲染循环便结束了，之后为我们就可以关闭应用程序了。
+		glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
+		glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
+	*/
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate(); //当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
 	return 0;
 }
+
