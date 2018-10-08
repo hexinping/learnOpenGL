9e899574b04a56915e7e13054845aa13cf3b44b3
hexinping <hexinping456@126.com>
2018-10-08 13:07:08 +0800

    opengl工程提交

---
+++ project/OpenGL/OpenGL/main.cpp
@@ -0,0 +1,12 @@
+
+#include "main.h"
+#include <iostream>
+#include "GLFW//glfw3.h"
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout << "1-------" << endl;
+	system("pause");
+	return 0;
+}
