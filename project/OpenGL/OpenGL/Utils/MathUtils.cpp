61cd5a4452bdda936959e8bce2d2811e968e692e
hexinping <hexinping456@126.com>
2018-10-19 17:37:41 +0800

    添加opengl数学工具类

---
+++ project/OpenGL/OpenGL/Utils/MathUtils.cpp
@@ -0,0 +1,56 @@
+#include "MathUtils.h"
+
+//glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);//第4个值默认设置为1
+// 译注：下面就是矩阵初始化的一个例子，如果使用的是0.9.9及以上版本
+// 下面这行代码就需要改为:
+// glm::mat4 trans = glm::mat4(1.0f)
+// 之后将不再进行提示
+
+static MathUtils *instance = nullptr;
+
+MathUtils * MathUtils::getInstance()
+{
+	if (!instance)
+	{
+		instance = new (std::nothrow) MathUtils();
+	}
+	return instance;
+}
+
+void MathUtils::translate(glm::vec4 *vec, glm::vec3 *transform)
+{
+	glm::mat4 trans; // 默认生成一个单位矩阵
+	trans = glm::translate(trans, *transform);
+	*vec = trans * (*vec);
+}
+
+
+void MathUtils::rotate(glm::vec4 *vec, float angle, glm::vec3 *axis)
+{
+	glm::mat4 trans;
+	trans = glm::rotate(trans, glm::radians(angle), *axis);
+	*vec = trans * (*vec);
+}
+
+void MathUtils::scale(glm::vec4 *vec, glm::vec3 *s)
+{
+	glm::mat4 trans;
+	trans = glm::scale(trans, *s);
+	*vec = trans * (*vec);
+}
+
+void MathUtils::setTranslateMat4(glm::mat4 *mat, glm::vec3 *transform)
+{
+	*mat = glm::translate(*mat, *transform);
+}
+
+
+void MathUtils::setRoateMat4(glm::mat4 *mat, float angle, glm::vec3 *axis)
+{
+	*mat = glm::rotate(*mat, glm::radians(angle), *axis);
+}
+
+void MathUtils::setSclaeMat4(glm::mat4 *mat, glm::vec3 *s)
+{
+	*mat = glm::scale(*mat, *s);
+}
---
+++ project/OpenGL/OpenGL/Utils/MathUtils.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+//#include <string>  
+//#include <fstream>  
+//#include <streambuf>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+using namespace std;
+class MathUtils
+{
+public:
+	static MathUtils * getInstance();
+
+	//位移一个向量
+	void translate(glm::vec4 *vec, glm::vec3 *transform);
+
+	//旋转：角度，旋转轴
+	void rotate(glm::vec4 *vec, float angle, glm::vec3 *axis);
+
+	//缩放
+	void scale(glm::vec4 *vec, glm::vec3 *s);
+
+	//位移变化矩阵
+	void setTranslateMat4(glm::mat4 *mat, glm::vec3 *transform);
+
+	//旋转变化矩阵
+	void setRoateMat4(glm::mat4 *mat, float angle, glm::vec3 *axis);
+
+	//缩放变化矩阵
+	void setSclaeMat4(glm::mat4 *mat, glm::vec3 *s);
+	
+
+};
+
