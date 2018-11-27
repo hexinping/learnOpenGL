0472540afaae106f61f9cf895507b480be12ed08
hexinping <hexinping456@126.com>
2018-11-27 15:56:31 +0800

    添加exe图标文件

--- project/BreakOut/BreakOut/main.cpp
+++ project/BreakOut/BreakOut/main.cpp
@@ -151,7 +151,7 @@ int createWindow(GLFWwindow** pWindow)
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 
